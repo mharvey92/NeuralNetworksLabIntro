d91697e1bba5c71b12e1eb3d4c3f0caac1bb7ae6
mharvey92 <ma.harvey1050@gmail.com>
2015-12-22 12:36:49 -0600

    initial commit

---
+++ NeuralNetworksIntro.R
@@ -0,0 +1,43 @@
+# This code is part of a lab that is taken from Louis Aslett's website
+
+library(nnet)
+
+cheese<-read.csv("cheese.csv")
+
+#data summary
+names(cheese)
+summary(cheese)
+head(cheese)
+
+#fit simple neural net
+fitnn1= nnet(taste ~ Acetic+H2S+Lactic, cheese, size=0, skip=TRUE, linout=TRUE)
+summary(fitnn1)
+
+#compare to linear model
+lm(taste ~ Acetic+H2S+Lactic, cheese)
+
+#computing Akaike Information Criterion given that sample size is small (n=30)
+SSE=sum(fitnn1$residuals^2)
+AIC=2*5+30*log(SSE/30)
+AIC
+
+#computing Schwartz Bayesian Information Criterion
+SBIC=5*log(30)+30*log(SSE/30)
+SBIC
+
+#create second model without variable "Acetic"
+fitnn2=nnet(taste~H2S+Lactic, cheese, size=0, skip=TRUE, linout=TRUE)
+summary(fitnn2)
+
+#compare fitnn1 to fitnn2
+SSE2=sum(fitnn2$residuals^2)
+AIC2=2*5+30*log(SSE2/30)
+AIC2<AIC
+
+#adding hidden layer w/ single node
+fitnn3=nnet(taste ~ Acetic +H2S+Lactic, cheese, size=1, linout=TRUE)
+
+#normalize data for robustness
+cheesescaled=scale(cheese)
+fitnn3=nnet(taste ~ Acetic +H2S+Lactic, cheesescaled, size=1, linout=TRUE)
+summary(fitnn3)
